976f5c9b41f1699bc012b0c61ba675469af24edc
incx07 <incx07@gmail.com>
2022-03-08 17:44:44 +0300

    add script to traine os.pipe()

---
+++ linux-bash/forc_exec/pipe.py
@@ -0,0 +1,24 @@
+'''Script to traine os.pipe() method'''
+import os
+
+
+r, w = os.pipe()
+pid = os.fork()
+
+command = b'echo'
+args = 'my string'
+
+if pid:
+    # This is the parent process
+    os.close(r)
+    print('Parent process is writing')
+    os.write(w, command)
+    print('Execute program:', command)
+    print('String to pass:', args)
+
+else:
+    # This is the child process
+    os.close(w)
+    print('Child Process is reading')
+    command = os.fdopen(r).read()
+    os.execlp(command, command, args)
