991267520f5ba4db19bff5c315c7ca8859373023
incx07 <incx07@gmail.com>
2022-03-08 18:55:03 +0300

    fix for bash task

--- linux-bash/arg_check.sh
+++ linux-bash/arg_check.sh
@@ -8,24 +8,27 @@
 #    print the 'cmdline' used to run this process, 
 #    if not a PID - add '1000' and print this sum to console.
 
-if [[ $# -eq 0 ]]; then
-    echo "No parameters found."
-elif [[ $# -ge 2 ]]; then
+
+if [[ $# -ne 1 ]]; then
     echo "$# parameters were passed. 1 is required."
 elif [[ -f $1 ]]; then
-    MSG=$(file -b "$1")
-    if [[ $MSG == *"ELF"*"executable"* ]]; then
-        readelf -p .rodata "$1"
+    file -b "$1" | grep -q '^ELF.*executable'
+    if [[ $? -eq 0 ]]; then
+        readelf -p .rodata $1
     elif [[ $1 == *".txt" ]]; then
         'date' >> "$1"
+    else
+    echo "Invalid parameter."
     fi
 elif [[ $1 =~ ^[0-9]+$ ]]; then
-    PID_LIST=$(ps -A -o pid)
-    if grep -q "^ *$1$" <<< "$PID_LIST"; then
+    ps -A -o pid | grep -q "^ *$1$"
+    if [[ $? -eq 0 ]]; then
         cat /proc/"$1"/cmdline
         echo -e
     else 
     SUM=$(( $1+1000 ))
     echo $SUM
     fi
+else
+echo "Invalid parameter."
 fi
