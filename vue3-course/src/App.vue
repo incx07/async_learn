95a59aa48b3c93c40abc573cc2308daeb78edeb9
incx07 <incx07@gmail.com>
2022-10-08 17:20:15 +0300

    Refactoring (adding separeted components)

--- vue3-course/src/App.vue
+++ vue3-course/src/App.vue
@@ -1,37 +1,26 @@
 <template>
     <div class="app">
-        <form @submit.prevent>
-            <h4>Create a post</h4>
-            <input 
-                v-bind:value="title" 
-                @input="title = $event.target.value" 
-                class="input" 
-                type="text" 
-                placeholder="Title"
-            >
-            <input 
-                v-bind:value="body" 
-                @input="body = $event.target.value" 
-                class="input" 
-                type="text" 
-                placeholder="Description"
-            >
-            <button 
-                class="btn" 
-                @click="createPost">Create
-            </button>
-        </form>
-
-        <div class="post" v-for="post in posts">
-            <div><strong>Title:</strong> {{ post.title }}</div>
-            <div><strong>Description:</strong> {{ post.body }}</div>
-        </div>
+        <PostForm
+            @create="createPost"       
+        ></PostForm>
+        <PostList 
+            :posts="posts"
+        ></PostList>
     </div>
 </template>
 
 
 <script>
+import PostForm from "./components/PostForm.vue";
+import PostList from "./components/PostList.vue";
+
+
 export default {
+    components: {
+    PostForm,
+    PostList
+},
+
     data() {
         return {
             posts: [
@@ -39,21 +28,12 @@ export default {
                 {id: 2, title: 'Post 2', body: 'Long description 2'},
                 {id: 3, title: 'Post 3', body: 'Long description 3'},
             ],
-            title: '',
-            body: '',
 
         }
     },
     methods: {
-        createPost() {
-            const newPost = {
-                id: Date.now(),
-                title: this.title,
-                body: this.body,
-            }
-            this.posts.push(newPost);
-            this.title = '';
-            this.body = '';
+        createPost(post) {
+            this.posts.push(post)
         },
     }
 }
@@ -70,27 +50,5 @@ export default {
 .app {
     padding: 20px;
 }
-.post {
-    padding: 15px;
-    border: 2px solid teal;
-    margin-top: 15px;
-}
-form {
-    display: flex;
-    flex-direction: column;
-}
-.input {
-    width: 100%;
-    border: 1px solid teal;
-    padding: 10px 15px;
-    margin-top: 15px;
-}
-.btn {
-    margin-top: 15px;
-    align-self: flex-end;
-    padding: 10px 15px;
-    background: none;
-    color: teal;
-    border: 1px solid teal;
-}
+
 </style>
---
+++ vue3-course/src/components/PostList.vue
@@ -0,0 +1,36 @@
+<template>
+    
+    <div>
+        <h3>Users List</h3>
+        <PostItem 
+            v-for="post in posts"
+            :post="post"
+            >        
+        </PostItem>
+    </div>
+
+</template>
+
+<script>
+import PostItem from '@/components/PostItem';
+export default {
+    components: {PostItem},
+    props: {
+        posts: {
+            type: Array,
+            required: true
+        }
+    }
+}
+
+
+</script>
+
+<style scoped>
+.post {
+    padding: 15px;
+    border: 2px solid teal;
+    margin-top: 15px;
+}
+    
+</style>
